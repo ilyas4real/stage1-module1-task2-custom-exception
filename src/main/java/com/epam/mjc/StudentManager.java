a0b6bd6f39ee1621e55d4927cf2aca60e66bdcba
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-03-20 20:39:16 +0100

    done

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,8 +5,21 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentNameException {
+    long val = 0;
+    boolean found = false;
+    for(int i = 0; i < IDs.length; i++) {
+      if (studentID == i) {
+        val = studentID;
+        found = true;
+    }
+    }
+    if (found == true) {
+      return Student.getValueOf(val);
+    } else {
+      throw new StudentNameException("Could not find student with ID " + studentID);
+    }
+
   }
 
   public static void main(String[] args) {
---
+++ src/main/java/com/epam/mjc/StudentNameException.java
@@ -0,0 +1,7 @@
+package com.epam.mjc;
+
+public class StudentNameException extends IllegalArgumentException{
+    public StudentNameException(String message) {
+        super(message);
+    }
+}
